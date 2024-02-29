39bc06c5fc274cbaa8f9db1e053155995079b638
Kanishka <kanishka1@myseneca.ca>
2024-02-29 00:36:18 -0500

    w4 added

---
+++ WORKSHOP4/lab/Fridge.cpp
@@ -0,0 +1,130 @@
+/***********************************************************************
+// OOP244 Workshop #4 p1
+//
+// File	Fridge.cpp
+// Version 1.0
+// Date: Summer 2023
+// Author Michael Huang
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name: -KANISHKA            Date: FEBRUARY 11, 2024            
+//
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+#include "Fridge.h"
+
+using namespace std;
+
+namespace seneca
+{
+
+    const char *Food::name() const
+    {
+        return m_name;
+    }
+    int Food::weight() const
+    {
+        return m_weight;
+    }
+    Food::Food() {}
+
+    Food::Food(const char *nm, int wei)
+    {
+        strncpy(m_name, nm, NAME_LEN);
+        m_weight = wei;
+    }
+
+    Fridge::Fridge()
+    {
+        m_numFoods = 0;
+        m_model = nullptr;
+        // m_foods = Food();
+    }
+
+    Fridge::Fridge(Food farr[], int nf, const char *mod)
+    {
+        Fridge();
+        if (nf > 0 && mod != nullptr && mod != '\0')
+        {
+            m_numFoods = nf;
+            m_model = new char[strlen(mod) + 1];
+            strcpy(m_model, mod);
+
+            for (int i = 0; i < nf && i < FRIDGE_CAP; i++)
+            {
+                m_foods[i] = farr[i];
+            }
+        }
+    }
+
+    Fridge::~Fridge()
+    {
+        delete[] m_model;
+    }
+
+    bool Fridge::addFood(const Food &f)
+    {
+        if (m_numFoods < FRIDGE_CAP)
+        {
+            m_foods[m_numFoods] = f;
+            m_numFoods++;
+            return true;
+        }
+
+        return false;
+    }
+
+    void Fridge::changeModel(const char *m)
+    {
+        if (m != nullptr || m != '\0')
+        {
+            delete m_model;
+            m_model = new char[strlen(m) + 1];
+            strcpy(m_model, m);
+        }
+    }
+
+    bool Fridge::fullFridge() const
+    {
+        if (m_numFoods >= FRIDGE_CAP)
+            return true;
+        else
+            return false;
+    }
+
+    bool Fridge::findFood(const char *f) const
+    {
+        for (int i = 0; i < m_numFoods; i++)
+        {
+            if (!strcmp(m_foods[i].name(), f))
+                return true;
+        }
+        return false;
+    }
+
+    std::ostream &Fridge::display(std::ostream &os) const
+    {
+        if (m_model == nullptr || *m_model == '\0')
+            return os;
+
+        os << "Fridge Model: " << m_model << endl;
+        os << "Food count: " << m_numFoods << " Capacity: " << FRIDGE_CAP << endl;
+        os << "List of Foods" << endl;
+
+        if (m_numFoods > 0)
+        {
+            for (int i = 0; i < m_numFoods; i++)
+            {
+
+                os << setw(NAME_LEN) << right << m_foods[i].name() << " | " << m_foods[i].weight() << endl;
+            }
+        }
+        return os;
+    }
+}
